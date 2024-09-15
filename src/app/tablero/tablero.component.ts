1c142ba637be85112fd92756b4f15c2548fd7448
HugoMD <hugomd2003xd@gmail.com>
2024-09-14 19:38:32 -0500

    musica y modal agregado

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JuegoService } from "../juego.service";
 import {NgIf, NgFor, NgClass} from "@angular/common";
 
@@ -10,16 +10,52 @@ import {NgIf, NgFor, NgClass} from "@angular/common";
   imports: [NgIf, NgFor, NgClass],
 })
 export class TableroComponent {
+  ganador: string | null = null;
+  showWinnerModal = false;
+  gameMusic = new Audio();
+  winMusic = new Audio();
+
   constructor(public juegoService: JuegoService) {}
+
+  ngOnInit(): void {
+    this.startGameMusic();
+  }
+
+  startGameMusic(): void {
+    this.gameMusic.src = '/assets/game-music.mp3';
+    this.gameMusic.loop = true;
+    this.gameMusic.play().catch(error => console.error('Error al reproducir música:', error));
+  }
+
+  stopGameMusic(): void {
+    this.gameMusic.pause();
+    this.gameMusic.currentTime = 0;
+  }
+
+  playWinMusic(): void {
+    this.winMusic.src = '/assets/win-music.mp3';
+    this.winMusic.play().catch(error => console.error('Error al reproducir música de victoria:', error));
+  }
 
   //Manejar clic en una columna
   handleColumnClick(column: number): void {
+    if(this.gameMusic.paused){
+      this.startGameMusic();
+    }
+
     if(this.juegoService.dropDisc(column)) {
       const winner = this.juegoService.checkWinner();
       if (winner) {
-        alert(`${winner} ha ganado! Gaaaaa`);
+        this.stopGameMusic();
+        this.playWinMusic();
+
+        this.ganador = winner;
+
+        setTimeout(() => {
+          this.showWinnerModal = true;
+        }, 500);
+
         this.juegoService.registerWinner(winner as 'Red' | 'Yellow');
-        this.juegoService.resetGame();
       } else {
         this.juegoService.switchPlayer();
       }
@@ -28,7 +64,15 @@ export class TableroComponent {
     }
   }
 
+  closeWinnerModal(): void {
+    this.showWinnerModal = false;
+    this.juegoService.resetGame();
+  }
+
   resetGame(): void {
+    this.winMusic.pause();
+    this.winMusic.currentTime = 0;
+    this.startGameMusic();
     this.juegoService.resetGame();
   }
 
