bae8408c97037ee4e3d47916ca03bcc36e1a8d17
HugoMD <hugomd2003xd@gmail.com>
2024-09-14 19:40:08 -0500

    musica y modal agregado

--- src/app/tablero/tablero.component.ts
+++ src/app/tablero/tablero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { JuegoService } from "../juego.service";
 import {NgIf, NgFor, NgClass} from "@angular/common";
 
